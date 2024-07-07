4cd13d19ab34b9f464efa3418de7df415270e075
Venkatareddyie <venkatareddy65852@gmail.com>
2024-07-07 03:10:40 -0500

    first commit

---
+++ src/main/java/com/jack/Mrjack/MrjackApplication.java
@@ -0,0 +1,13 @@
+package com.jack.Mrjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrjackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jack/Mrjack/MrjackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.Mrjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrjackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
